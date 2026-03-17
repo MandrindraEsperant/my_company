4714ab7c050b4a4f57b06b412a65d482ac86b4f7
RANDRIAMAHASOA Mandrindra Espérant <mandrindraesperant@gmail.com>
2026-03-17 18:59:45 +0300

    update config next

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // INDISPENSABLE pour créer le dossier /out
   images: {
     unoptimized: true,
   },
   typescript: {
-    ignoreBuildErrors: true, // Vos options du fichier .mjs
+    ignoreBuildErrors: true,
   },
 };
 
